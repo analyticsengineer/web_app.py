e3033e8f059ea9577059788b1f8fe641fd724f9c
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-06-29 00:34:08 +0100

    Create Data-Summary.py

---
+++ ~/app/Data-Summary.py
@@ -0,0 +1,55 @@
+from PIL import Image
+import streamlit as st
+from pandas_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+from openpyxl import load_workbook
+
+# setting image
+image = Image.open('image4.png')
+
+
+col1, col2 = st.columns(2)
+
+col1.header("Simple Data Analysis Web App")
+title_1 =  '<p style="font-family:sans-serif; color:Grey;">Visualize the TOP TEN Insights fom your dataset</p>'
+col1.markdown(title_1, unsafe_allow_html=True)
+col2.image(image)
+
+# Setting menu visibility
+st.markdown(""" <style>
+#Mainmenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>""", unsafe_allow_html=True)
+
+df_file = st.file_uploader("Upload your file: ", type=['csv', 'xlsx', 'pickle'])
+
+# Open Csv File
+try:
+  df_file = pd.read_csv(df_file)
+  st.markdown("Your Data Record: ")
+  st.dataframe(df_file)
+        
+except:
+  csv = '<p style="font-family: Quicksand_medium; color:#c28080; font-size: 20px;">Please Load A CSV, EXCEL Or ' \
+           'PICKLE File To Begin</p>'
+  st.markdown(csv, unsafe_allow_html=True)
+
+# Open Excel File
+try:
+  df_file = pd.read_excel(df_file, engine='openpyxl')
+  st.markdown("Your Data Record: ")
+  st.dataframe(df_file)     
+except:
+  pass
+
+# Read Pickle File
+try:
+  df_file = pd.read_pickle(df_file)
+  st.markdown("Your Data Record: ")
+  AgGrid(df_file, editable=True)
+except:
+  pass
+
+data_summary = st.button("Data Summary")
+if data_summary:
+  st_profile_report(df_file)

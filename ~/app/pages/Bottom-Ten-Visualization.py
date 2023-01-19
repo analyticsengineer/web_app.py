d732e6ec085faedfcdaad06cdf8978845931ad7b
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2023-01-19 18:21:39 +0100

    Update Bottom-Ten-Visualization.py

--- ~/app/pages/Bottom-Ten-Visualization.py
+++ ~/app/pages/Bottom-Ten-Visualization.py
@@ -76,22 +76,22 @@ if type == 'Color':
         plotType_color = st.selectbox("Plot Type:", ['Choose', 'Line', 'Bar', 'Pie'])
         if plotType_color == 'Line':
             fig = px.line(df_file, x=df_file[cols4], y=df_file[cols3])
-            st.plotly_chart(fig, use_container_width=True)
+            st.plotly_chart(fig, use_container_width=True,theme="streamlit",  # ✨New Features!)
         if plotType_color == 'Pie':
             fig = px.pie(names=df_file[cols4], values=df_file[cols3])
-            st.plotly_chart(fig, use_container_width=True)
+            st.plotly_chart(fig, use_container_width=True,theme="streamlit",  # ✨New Features!)
         if plotType_color == 'Bar':
             fig = px.bar(df_file, x=df_file[cols4], y=df_file[cols3], color=df_file[cols4])
-            st.plotly_chart(fig, use_container_width=True)
+            st.plotly_chart(fig, use_container_width=True,theme="streamlit",  # ✨New Features!)
             
 if type == 'No Color':
         plotType_nocolor = st.selectbox("Plot Type:", ['Choose', 'Line', 'Bar', 'Pie'])
         if plotType_nocolor == 'Line':
             fig = px.line(df_file, x=df_file[cols4], y=df_file[cols3])
-            st.plotly_chart(fig, use_container_width=True)
+            st.plotly_chart(fig, use_container_width=True,theme="streamlit",  # ✨New Features!)
         if plotType_nocolor == 'Pie':
             fig = px.pie(names=df_file[cols4], values=df_file[cols3])
-            st.plotly_chart(fig, use_container_width=True)
+            st.plotly_chart(fig, use_container_width=True,theme="streamlit",  # ✨New Features!)
         if plotType_nocolor == 'Bar':
             fig = px.bar(df_file, x=df_file[cols4], y=df_file[cols3])
-            st.plotly_chart(fig, use_container_width=True)
+            st.plotly_chart(fig, use_container_width=True,theme="streamlit",  # ✨New Features!)
